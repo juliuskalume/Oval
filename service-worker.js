1bace57416696fd038e42cb995f2820baf0623dc
juliuskalume <juliuskalume906@gmail.com>
2026-04-04 20:59:42 +0300

    Respect iPhone safe areas in PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v3";
+const CACHE_VERSION = "v4";
 const STATIC_CACHE = `oval-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `oval-runtime-${CACHE_VERSION}`;
 const OFFLINE_URL = "./offline.html";
